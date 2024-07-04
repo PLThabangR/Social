e10c2a3b55a11ac62693622a53fe3480e7d26ba7
PLThabangR <rakgoropothabang@gmail.com>
2024-07-04 05:20:44 +0200

    adding follow and unfollower logic using push and pull from mongoose

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -18,3 +18,52 @@ try{
     })
 }
 }
+
+
+export const followUnfollow = async (req,res)=>{
+    try{
+        const {id} = req.params
+
+        //find user to follow by id
+        const userToModify = await UserModal.findById(id)
+        //Find information about my self
+        const currentUser = await UserModal.findById(req.user._id)
+        
+        //Check if user trying to follow himself
+        //set req.user._id to string as it is an  object
+        if(id===req.user._id.toString()){
+            return res.status(404).json({error:"You cannot follow/unfollow yourself"})
+        }
+        //Check if user is found
+        if(!userToModify || !currentUser){
+            return res.status(404).json({error:"User not found"})
+        }
+
+        //Check if the  following array includes the  id of user to modify
+        const isFollowing = currentUser.following.includes(id)
+        
+        if(isFollowing){
+            //unfollow user 
+            //Update the arrays followers and follows array of both users using $pull
+            await UserModal.findByIdAndUpdate(id,{$pull:{followers:req.user._id}})
+            await UserModal.findByIdAndUpdate(req.user._id,{$pull:{following:id}})
+            res.status(200).json({message:"User unfollowed successfully"})
+
+
+        }else{
+            //follow user
+            //Update the arrays followers and follows array of both users using $push 
+            await UserModal.findByIdAndUpdate(id,{$push:{followers:req.user._id}})
+            await UserModal.findByIdAndUpdate(req.user._id,{$push:{following:id}})
+            res.status(200).json({message:"User followed successfully"})
+        }
+
+
+
+    }catch(error){
+        console.log("Error from folowUnfollow",error.message)
+        res.status(500).json({
+            error:error.message
+        })
+    }
+}
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,12 +1,12 @@
 import express from "express"
 import { protectedRoute } from "../Middleware/protectedRoute.js";
-import {getUserProfile} from "../controllers/userController.js"
+import {getUserProfile,followUnfollow} from "../controllers/userController.js"
 
 const router = express.Router()
 
 router.get("/profile/:username",protectedRoute,getUserProfile)
 // router.get("/suggested",protectedRoute,getUserProfile)
-// router.get("/follow/:id",protectedRoute,followUnfollow)
+router.get("/follow/:id",protectedRoute,followUnfollow)
 // router.post("/update",protectedRoute,updateUSerProfile)
 
 
