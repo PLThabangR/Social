e798b1eb156751c80ef0b77ee27cd4834a4f897a
PLThabangR <rakgoropothabang@gmail.com>
2024-07-08 01:38:31 +0200

    Designing the frontend

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home/Home"
 import SignUp from "./pages/auth/SignUp"
 import Login from "./pages/auth/Login"
+import SideBar from "./components/common/SideBar"
+import RightPanel from "./components/common/RightPanel"
 
 function App() {
  
@@ -9,12 +11,14 @@ function App() {
   return (
     <>
     <div className="flex max-w-6xl mx-auto">
+      {/*This is a common component*/ }
+      <SideBar/>
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/signup" element={<SignUp/>}/>
       <Route path="/login" element={<Login/>}/>
     </Routes>
-
+<RightPanel/>
 
     </div>
     </>
