b82cf9eba5b18e3f9d89f5b48c74da263222fa57
PLThabangR <rakgoropothabang@gmail.com>
2024-07-07 23:53:11 +0200

    daisy ui theme setup for the app completed

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -1,4 +1,5 @@
 import daisyui from "daisyui"
+import daisyUIThemes from "daisy/src/theming/themes";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
@@ -8,5 +9,18 @@ export default {
   plugins: [
     daisyui,
   ],
+  daisyui: {
+    themes: ["light", 
+      {
+        black:{
+          ...daisyUIThemes["black"],
+          primary:"rgb(29,155,240)",
+          secondary:"rgb(24,24,24)"
+
+        }
+      }
+
+    ],
+  },
 }
 
