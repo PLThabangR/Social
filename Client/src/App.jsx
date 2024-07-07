4ab62e90651d8b48473d40595b940c703a9794a1
PLThabangR <rakgoropothabang@gmail.com>
2024-07-08 00:38:07 +0200

    SignUp page design using daisyUI

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,22 @@
-
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home/Home"
+import SignUp from "./pages/auth/SignUp"
+import Login from "./pages/auth/Login"
 
 function App() {
  
 
   return (
     <>
-  
+    <div className="flex max-w-6xl mx-auto">
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/signup" element={<SignUp/>}/>
+      <Route path="/login" element={<Login/>}/>
+    </Routes>
+
+
+    </div>
     </>
   )
 }
